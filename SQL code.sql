40c517aa89f5e0460f7621a07b22c91723cef843
Alex <112234421+adallan@users.noreply.github.com>
2023-01-12 13:10:26 -0800

    Update SQL code.sql

--- SQL code.sql
+++ SQL code.sql
@@ -1,32 +1,32 @@
 Step 1: Creation of Temp tables to be combined into a master table
-Step 2 & 3: Making a ride_length column as well as transforming timestamp into weekdays,dates and time 
+Steps 2 & 3: Make a ride_length column as well as transform the timestamp into weekdays, dates and time 
 Also intentionally did not select columns: long/lat start and ends, ride_id, station_id, end_station_id so they would be filtered out when making tables
 ---------------------------------------------------------------------------------------
 
 SELECT 
  DATE_DIFF(ended_at,started_at,MINUTE) AS ride_length, 
  
- -- Creates the length of a rides as a column in minutes
+ -- Creates the length of a ride as a column in minutes.
  
  FORMAT_DATE('%A',started_at) AS start_day,FORMAT_DATE('%A',ended_at) AS end_day,
  
- -- Formats timestamp into weekdays in name (example "Wednesday") and renames column
+ -- Formats timestamp into weekdays in a name (for example "Wednesday") and renames column.
  
  EXTRACT(TIME FROM ended_at) AS end_time ,EXTRACT(TIME FROM started_at) AS start_time,
  
- -- Extracts time relative information from the timestamp  
+ -- Extracts time relative information from the timestamp. 
  
  EXTRACT(DATE FROM ended_at) AS end_date, EXTRACT(DATE FROM started_at) AS start_date,
  
- -- Extracts date (YYYY-MM-DD) from timestamp and renames column
+ -- Extracts date (YYYY-MM-DD) from timestamp and renames column.
  
   member_casual, start_station_name,rideable_type
 FROM buoyant-genre-370301.Cyclistic_oct2021_oct2022.Divvy_tripdata_oct2021
 UNION ALL
 
--- Fully combines the other querries
+-- Fully combines the other queries.
 
---Repeat for other tables
+--Repeat for other tables.
 SELECT
  DATE_DIFF(ended_at,started_at,MINUTE) AS ride_length,
  FORMAT_DATE('%A',started_at) AS start_day,FORMAT_DATE('%A',ended_at) AS end_day,
@@ -51,11 +51,11 @@ SELECT
  member_casual, start_station_name,rideable_type
 FROM buoyant-genre-370301.Cyclistic_oct2021_oct2022.Divvy_tripdata_jan2022
 
--- At the end of querry used bigquerrys 'save as' function to create a table of the unioned data tables
--- Repeated this twice to make other 2 data chunks
+-- At the end of the query used Bigquerrys 'save as' function to create a table of the unioned data tables.
+-- Repeated this twice to make the other 2 data chunks.
 
 
-Step 4: Union of temp tables into master data set
+Step 4: Union of temp tables into a master data set
 ----------------------------------------------------------------------------------------------------------
 SELECT *
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.feb_march_april_sept_oct2022`
@@ -66,33 +66,33 @@ UNION ALL
 SELECT *
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.may_june_july_aug2022`
 
--- Utilized bigquerry's 'save as' function to create new table named master_table_oct2021_2022
+-- Utilized Bigquerry's 'save as' function to create a new table named master_table_oct2021_2022
 
-Step 5: Querring to Find negetative ride_lengths 
+Step 5: Querying to Find negative ride_lengths 
 Step 6: Deleting negative row values
 ----------------------------------------------------------------------------------------------------------
 SELECT ride_length
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 WHERE ride_length < 0
 
--- Finds all rows containting rides that are under 0 minutes long
+-- Finds all rows containing rides that are under 0 minutes long
 
 DELETE
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 WHERE ride_length < 0
 
--- Only deletes querried results, which are all found negative ride values
+-- Only deletes queried results, which are all found negative ride values
 
 Step 7: Data aggregation for Data visualization 
 -----------------------------------------------------------------------------------------------------------
 
--- Counts number of trips by weekday with member type
+-- Counts the number of trips by weekday with member type
 
 SELECT member_casual, COUNT(ride_length) as trips, start_day
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 GROUP BY member_casual, start_day
 
--- Counts number of trips and Average ride_lengths for each bike type with member type
+-- Counts the number of trips and Average ride_lengths for each bike type with member type
 
 SELECT AVG(ride_length) AS avg_ridelength,member_casual,rideable_type, COUNT(ride_length) as trips
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
@@ -111,9 +111,9 @@ ORDER BY station_usage DESC
 SELECT approx_top_count(start_day, 7) as usage_weekday, member_casual
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 GROUP BY member_casual
--- This querry is not uploaded to Tableau, visualization results are retrieved from the JOINS of trips per weekday view and the station usage view
+-- This query is not uploaded to Tableau, visualization results are retrieved from the JOINS of trips per weekday view and the station usage view
 
--- Counts number of trips by date with member type
+-- Counts the number of trips by date with member type
 
 SELECT member_casual, COUNT(ride_length) as trips, start_date
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
@@ -126,7 +126,7 @@ FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 GROUP BY member_casual, start_day, start_date
 ORDER BY total_trip_times DESC
 
--- Counts number of trips made before noon and after noon
+-- Counts the number of trips made before noon and afternoon
 SELECT COUNT(ride_length) as trips, member_casual
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 WHERE start_time < "12:00:00"
@@ -138,4 +138,5 @@ WHERE start_time > "12:00:00"
 GROUP BY member_casual
 -- Afternoon
 
--- All  querries above in step 7 are saved as a view, then exported to Google Sheets to be later imported into Tableau for visualization
+-- All  queries above in step 7 are saved as a view, then exported to Google Sheets to be later imported into Tableau for visualization
+
