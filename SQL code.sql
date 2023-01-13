c485689df3aa07a7340a2438c502e2e5db51797d
Alex <112234421+adallan@users.noreply.github.com>
2023-01-12 17:21:31 -0800

    Update SQL code.sql

--- SQL code.sql
+++ SQL code.sql
@@ -1,6 +1,6 @@
-Step 1: Creation of Temp tables to be combined into a master table
-Steps 2 & 3: Make a ride_length column as well as transform the timestamp into weekdays, dates and time 
-Also intentionally did not select columns: long/lat start and ends, ride_id, station_id, end_station_id so they would be filtered out when making tables
+Step 1: Creation of Temp tables to be combined into a master table.
+Steps 2 & 3: Make a ride_length column as well as transform the timestamp into weekdays, dates and time.
+Also intentionally did not select columns: long/lat start and ends, ride_id, station_id, end_station_id so they would be filtered out when making tables.
 ---------------------------------------------------------------------------------------
 
 SELECT 
@@ -66,7 +66,7 @@ UNION ALL
 SELECT *
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.may_june_july_aug2022`
 
--- Utilized Bigquerry's 'save as' function to create a new table named master_table_oct2021_2022
+-- Utilized Bigquerry's 'save as' function to create a new table named master_table_oct2021_2022.
 
 Step 5: Querying to Find negative ride_lengths 
 Step 6: Deleting negative row values
@@ -75,13 +75,13 @@ SELECT ride_length
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 WHERE ride_length < 0
 
--- Finds all rows containing rides that are under 0 minutes long
+-- Finds all rows containing rides that are under 0 minutes long.
 
 DELETE
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 WHERE ride_length < 0
 
--- Only deletes queried results, which are all found negative ride values
+-- Only deletes queried results, which are all found negative ride values.
 
 Step 7: Data aggregation for Data visualization 
 -----------------------------------------------------------------------------------------------------------
@@ -92,51 +92,51 @@ SELECT member_casual, COUNT(ride_length) as trips, start_day
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 GROUP BY member_casual, start_day
 
--- Counts the number of trips and Average ride_lengths for each bike type with member type
+-- Counts the number of trips and Average ride_lengths for each bike type with member type.
 
 SELECT AVG(ride_length) AS avg_ridelength,member_casual,rideable_type, COUNT(ride_length) as trips
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 GROUP BY member_casual, rideable_type
 
--- Counts each usage of station_names, Total ride times in each station with member type
+-- Counts each usage of station_names, Total ride times in each station with member type.
 
 SELECT COUNT(start_station_name) AS station_usage, SUM(ride_length) AS total_time, member_casual, start_station_name
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 GROUP BY member_casual, start_station_name
 ORDER BY station_usage DESC
--- Shows most used station first
+-- Shows most used station first.
 
--- Creates Mode for weekdays with member type
+-- Creates Mode for weekdays with member type.
 
 SELECT approx_top_count(start_day, 7) as usage_weekday, member_casual
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 GROUP BY member_casual
--- This query is not uploaded to Tableau, visualization results are retrieved from the JOINS of trips per weekday view and the station usage view
+-- This query is not uploaded to Tableau, visualization results are retrieved from the JOINS of trips per weekday view and the station usage view.
 
--- Counts the number of trips by date with member type
+-- Counts the number of trips by date with member type.
 
 SELECT member_casual, COUNT(ride_length) as trips, start_date
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 GROUP BY member_casual, start_date
 
--- Total ride lengths by date with member type
+-- Total ride lengths by date with member type.
 
 SELECT member_casual, SUM(ride_length) as total_trip_times, start_day, start_date
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022`
 GROUP BY member_casual, start_day, start_date
 ORDER BY total_trip_times DESC
 
--- Counts the number of trips made before noon and afternoon
+-- Counts the number of trips made before noon and afternoon.
 SELECT COUNT(ride_length) as trips, member_casual
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 WHERE start_time < "12:00:00"
 GROUP BY member_casual
--- Before noon
+-- Before noon.
 SELECT COUNT(ride_length) as trips, member_casual
 FROM `buoyant-genre-370301.Cyclistic_oct2021_oct2022.master_table_oct2021_2022` 
 WHERE start_time > "12:00:00"
 GROUP BY member_casual
--- Afternoon
+-- Afternoon.
 
--- All  queries above in step 7 are saved as a view, then exported to Google Sheets to be later imported into Tableau for visualization
+-- All  queries above in step 7 are saved as a view, then exported to Google Sheets to be later imported into Tableau for visualization.
 
